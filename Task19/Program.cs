72aa54c198b75e82f9e34a10d17eedab669635ec
Valeriy2077 <84186810+Valeriy2077@users.noreply.github.com>
2023-01-27 01:48:01 +0300

    task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+System.Console.WriteLine("Введите число");
+
+string? text = Console.ReadLine();
+
+void CheckingNumber(string num)
+
+{
+    if (num[0]==num[4] || num[1]==num[3]){
+
+    Console.WriteLine($"Ваше число: {num} - Палиндром");
+
+  }
+
+  else Console.WriteLine($"Ваше число: {num} - Не палиндром");
+    }
+
+    if (text!.Length == 5)
+    {
+
+  CheckingNumber(text);
+
+    }
+
+else Console.WriteLine("Введено не правильное число");
