4bbefc2d5764685a2c6e08287bdc2174e46395ae
Valeriy2077 <84186810+Valeriy2077@users.noreply.github.com>
2023-01-27 18:00:30 +0300

    Task23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -5,3 +5,31 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
+Console.Write("Введите число N: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube)
+
+{
+  int answer = 1;
+  int length = cube.Length;
+  while (answer <  length){
+    cube[answer] = Convert.ToInt32(Math.Pow(answer, 3));
+    answer++;
+  }
+}
+
+void PrintNum(int[] coll)
+
+{
+  int count = coll.Length;
+  int index = 1;
+  while(index < count){
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+
+int[] num = new int[cube+1];
+Cube(num);
+PrintNum(num);
