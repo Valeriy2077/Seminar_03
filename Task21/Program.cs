e2396e2532a75ad10d0080389699c29d5ced0b47
Valeriy2077 <84186810+Valeriy2077@users.noreply.github.com>
2023-01-27 03:22:15 +0300

    /

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -6,21 +6,3 @@
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-double[] coordsA = new double[2];
-double[] coordsB = new double[2];
-// double[] 
-
-System.Console.WriteLine("Введите координату X точки A:");
-coordsA[0] = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Введите координату Y точки A:");
-coordsA[1] = Convert.ToInt32(Console.ReadLine());
-
-System.Console.WriteLine("Введите координату X точки B:");
-coordsA[0] = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Введите координату Y точки B:");
-coordsA[1] = Convert.ToInt32(Console.ReadLine());
-
-double S = Math.Sqrt(Math.Pow (coordsA[1] - coordsB[1],2) +
-     Math.Pow (coordsA[0] - coordsB[0],2));
-
-System.Console.WriteLine("Расстояние {}");
